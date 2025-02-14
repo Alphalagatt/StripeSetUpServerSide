6b8678f52e1505065ac5d758ee14574c29bdbfa2
Alpha Lagatt <lagattalpha@gmail.com>
2025-02-14 11:40:44 +1100

    stripe for TPI Init

---
+++ src/functions/paymentIntent.js
@@ -0,0 +1,26 @@
+const { app } = require('@azure/functions');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+app.setup({ enableHttpStream: true });
+
+app.http('paymentIntent', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+        
+        console.log(request.body);
+
+        //const {items} = request.body;
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: 1000,
+            currency: 'aud',
+            automatic_payment_methods: {
+                enabled: true,
+            },
+        });
+
+        return {clientSecret: paymentIntent.client_secret};
+    }
+});
+
