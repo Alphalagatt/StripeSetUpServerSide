db756fefe3c0d27d3296094d17e7561b3db94e58
Alpha Lagatt <lagattalpha@gmail.com>
2025-02-14 15:52:42 +1100

    test10

--- src/functions/paymentIntent.js
+++ src/functions/paymentIntent.js
@@ -4,18 +4,18 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 app.setup({ enableHttpStream: true });
 
 app.http('paymentIntent', {
-    methods: ['GET','POST'],
+    methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     headers: {
         "Access-Control-Allow-Origin": "*",  // Allow all origins (or specify your domain)
     },
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
-        
+
         context.log(process.env.STRIPE_SECRET_KEY);
 
         //const {items} = request.body;
-        
+
         const paymentIntentVal = await stripe.paymentIntents.create({
             amount: 1000,
             currency: 'aud',
@@ -26,9 +26,14 @@ app.http('paymentIntent', {
 
         context.log(paymentIntentVal.client_secret);
         const clientSecret = await paymentIntentVal.client_secret;
-        return {"Name":"Hello World"};
+        return {
+            jsonBody: {
+                Name: 'Hello World',
+                clientSecret: clientSecret
+            }
+        };
         //return {clientSecret: clientSecret};
-        
+
     }
 });
 
