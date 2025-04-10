3d671e0a3ef356d5af1507ef063fdaaa7ef966bd
Alpha Lagatt <lagattalpha@gmail.com>
2025-04-10 16:30:30 +1000

    update 2 10/04

--- src/functions/paymentIntent.js
+++ src/functions/paymentIntent.js
@@ -1,80 +1,48 @@
-const { app } = require('@azure/functions');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-app.setup({ enableHttpStream: true });
+async function createPaymentIntent(req, res) {
+    res.log("Creating PaymentIntent...");
+    res.log("Request body: ", req.params);
+    //res.log(res);
+    
+    const amount = req.params.amount;
+    const invoiceNumber = req.params.invoiceNumber;
+    const invoiceDescription = req.params.invoiceDescription;
 
-app.http('paymentIntent', {
-    methods: ['GET','POST'],
-    authLevel: 'anonymous',
-    headers: {
-        "Access-Control-Allow-Origin": "*",  // Allow all origins (or specify your domain)
-    },
-    handler: (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
-
-        context.log(process.env.STRIPE_SECRET_KEY);
-        context.log("request: ", request);
-        context.log("context: ", context);
-
-        const { amount, invoiceNumber, invoiceDescription } = request.params;
-
-        /*
-
-        if (!amount || isNaN(amount)) {
-            return {
-                jsonBody: {
-                    body: "Amount is required and should be a valid number"
-                }
-            }
-
-        };
-
-
-        if (!invoiceNumber || !invoiceDescription) {
-            return {
-                jsonBody: {
-                    status: 400,
-                    body: "Invoice number and description are required"
-                }
-            };
+    res.log("Amount: ", amount);
+    
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount * 100,
+        currency: 'aud',
+        automatic_payment_methods: {
+            enabled: true,
+        },
+        metadata: {
+            invoiceNumber: invoiceNumber,
+            invoiceDescription: invoiceDescription
         }
+    });
 
+    
+    res.log("PaymentIntent created: ", paymentIntent);
 
-        // Create a PaymentIntent with the specified amount, currency, and metadata
-        /*
-                stripe.paymentIntents.create({
-                    amount: amount * 100,
-                    currency: 'aud',
-                    automatic_payment_methods: {
-                        enabled: true,
-                    },
-                    metadata: {
-                        invoiceNumber: invoiceNumber,
-                        invoiceDescription: invoiceDescription
-                    }
-                }).then((paymentIntent) => {
-                    context.log("PaymentIntent created: ", paymentIntent);
-                    return paymentIntent;
-                    
-                }).catch((error) => {
-                    context.log("Error creating PaymentIntent: ", error);
-                    context.res = {
-                        status: 500,
-                        body: "Error creating PaymentIntent"
-                    };
-                });
-        */
-        //context.log(paymentIntentVal.client_secret);
-        //const clientSecret = await paymentIntentVal.client_secret;
-        return {
-            jsonBody: {
-                Name: 'Hello World',
-                //request: request,
-                //context: context,
-            }
-        };
-        //return {clientSecret: clientSecret};
+    
+    res.res = {
+        status: 200,
+        body: {
+            message: "PaymentIntent created successfully",
+            amount: amount,
+            paymentIntent: paymentIntent.client_secret,
+        },
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+    //res.log("Response: ", res.res);
+    return res.res;
+   
+    
+};
 
-    }
-});
+module.exports = createPaymentIntent;
 
--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@ const { app } = require('@azure/functions');
 app.setup({
     enableHttpStream: true,
 });
+
+const paymentIntent = require('./functions/paymentIntent');
+
+app.http('paymentIntent', {
+    methods: ['POST'],
+    authLevel: 'anonymous',
+    handler: paymentIntent,
+});
+
