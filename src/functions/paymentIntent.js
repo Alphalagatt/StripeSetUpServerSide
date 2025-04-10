4894ea81c54f5dcda4123de15faefe7b445121bc
Alpha Lagatt <lagattalpha@gmail.com>
2025-04-10 13:48:47 +1000

    update 1 10/4

--- src/functions/paymentIntent.js
+++ src/functions/paymentIntent.js
@@ -4,59 +4,73 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 app.setup({ enableHttpStream: true });
 
 app.http('paymentIntent', {
-    methods: ['GET', 'POST'],
+    methods: ['GET','POST'],
     authLevel: 'anonymous',
     headers: {
         "Access-Control-Allow-Origin": "*",  // Allow all origins (or specify your domain)
     },
-    handler: async (request, context) => {
+    handler: (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
 
         context.log(process.env.STRIPE_SECRET_KEY);
-        console.log("request: ",request.body);
-        console.log("context: ",context);
+        context.log("request: ", request);
+        context.log("context: ", context);
 
-        //const {items} = 
-        // Retrieve the amount, invoice number, and description from the request body
-        const { amount, invoiceNumber, invoiceDescription } = request.body;
+        const { amount, invoiceNumber, invoiceDescription } = request.params;
+
+        /*
 
         if (!amount || isNaN(amount)) {
-            context.res = {
-                status: 400,
-                body: "Amount is required and should be a valid number"
-            };
-            return;
-        }
+            return {
+                jsonBody: {
+                    body: "Amount is required and should be a valid number"
+                }
+            }
+
+        };
+
 
         if (!invoiceNumber || !invoiceDescription) {
-            context.res = {
-                status: 400,
-                body: "Invoice number and description are required"
+            return {
+                jsonBody: {
+                    status: 400,
+                    body: "Invoice number and description are required"
+                }
             };
-            return;
         }
 
 
         // Create a PaymentIntent with the specified amount, currency, and metadata
-
-        const paymentIntentVal = await stripe.paymentIntents.create({
-            amount: amount * 100,
-            currency: 'aud',
-            automatic_payment_methods: {
-                enabled: true,
-            },
-            metadata: {
-                invoiceNumber: invoiceNumber,
-                invoiceDescription: invoiceDescription
-            }
-        });
-
-        context.log(paymentIntentVal.client_secret);
-        const clientSecret = await paymentIntentVal.client_secret;
+        /*
+                stripe.paymentIntents.create({
+                    amount: amount * 100,
+                    currency: 'aud',
+                    automatic_payment_methods: {
+                        enabled: true,
+                    },
+                    metadata: {
+                        invoiceNumber: invoiceNumber,
+                        invoiceDescription: invoiceDescription
+                    }
+                }).then((paymentIntent) => {
+                    context.log("PaymentIntent created: ", paymentIntent);
+                    return paymentIntent;
+                    
+                }).catch((error) => {
+                    context.log("Error creating PaymentIntent: ", error);
+                    context.res = {
+                        status: 500,
+                        body: "Error creating PaymentIntent"
+                    };
+                });
+        */
+        //context.log(paymentIntentVal.client_secret);
+        //const clientSecret = await paymentIntentVal.client_secret;
         return {
             jsonBody: {
                 Name: 'Hello World',
-                clientSecret: clientSecret
+                //request: request,
+                //context: context,
             }
         };
         //return {clientSecret: clientSecret};
